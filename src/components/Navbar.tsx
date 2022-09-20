f346f67041e7563f2f05d379d3c26d8df2feeb8e
MatveyRubets <rmatvey11@gmail.com>
2022-09-20 19:38:53 +0200

    localstorage work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Container, Navbar as NavbarBs, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const Navbar = () => {
+	const { openCart, cartQuantity } = useShoppingCart();
+
 	return (
 		<NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
 			<Container>
@@ -17,66 +20,70 @@ const Navbar = () => {
 						About
 					</Nav.Link>
 				</Nav>
-				<Button
-					style={{ width: "3rem", height: "3rem", position: "relative" }}
-					className="rounded"
-					variant="outline-primary"
-				>
-					<svg
-						version="1.1"
-						id="Layer_1"
-						xmlns="http://www.w3.org/2000/svg"
-						x="0px"
-						y="0px"
-						viewBox="0 0 476.944 476.944"
+				{cartQuantity > 0 && (
+					<Button
+						onClick={openCart}
+						style={{ width: "3rem", height: "3rem", position: "relative" }}
+						className="rounded"
+						variant="outline-primary"
 					>
-						<g>
-							<path
-								d="M84.176,379.875c-26.762,0-48.535,21.772-48.535,48.534s21.772,48.534,48.535,48.534c26.762,0,48.534-21.772,48.534-48.534
+						<svg
+							version="1.1"
+							id="Layer_1"
+							xmlns="http://www.w3.org/2000/svg"
+							x="0px"
+							y="0px"
+							viewBox="0 0 476.944 476.944"
+						>
+							<g>
+								<path
+									d="M84.176,379.875c-26.762,0-48.535,21.772-48.535,48.534s21.772,48.534,48.535,48.534c26.762,0,48.534-21.772,48.534-48.534
 		S110.938,379.875,84.176,379.875z M84.176,446.944c-10.22,0-18.535-8.314-18.535-18.534s8.314-18.534,18.535-18.534
 		c10.22,0,18.534,8.314,18.534,18.534S94.396,446.944,84.176,446.944z"
-							/>
-							<path
-								d="M342.707,379.875c-26.762,0-48.534,21.772-48.534,48.534s21.772,48.534,48.534,48.534
+								/>
+								<path
+									d="M342.707,379.875c-26.762,0-48.534,21.772-48.534,48.534s21.772,48.534,48.534,48.534
 		c26.762,0,48.535-21.772,48.535-48.534S369.469,379.875,342.707,379.875z M342.707,446.944c-10.22,0-18.534-8.314-18.534-18.534
 		s8.314-18.534,18.534-18.534c10.22,0,18.535,8.314,18.535,18.534S352.927,446.944,342.707,446.944z"
-							/>
-							<path
-								d="M413.322,0l-9.835,60H1.999l28.736,175.88c4.044,24.67,26.794,43.995,51.794,43.995h284.917l-6.557,40H50.642v30h335.73
+								/>
+								<path
+									d="M413.322,0l-9.835,60H1.999l28.736,175.88c4.044,24.67,26.794,43.995,51.794,43.995h284.917l-6.557,40H50.642v30h335.73
 		L438.804,30h36.141V0H413.322z M372.363,249.875H82.529c-10.174,0-20.543-8.808-22.188-18.841L37.298,90h361.271L372.363,249.875z"
-							/>
-						</g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-						<g></g>
-					</svg>
-					<div
-						className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-						style={{
-							color: "white",
-							width: "1.25rem",
-							height: "1.25rem",
-							position: "absolute",
-							bottom: 0,
-							right: 0,
-							transform: "translate(25%, 25%)",
-						}}
-					>
-						3
-					</div>
-				</Button>
+								/>
+							</g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+							<g></g>
+						</svg>
+
+						<div
+							className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+							style={{
+								color: "white",
+								width: "1.25rem",
+								height: "1.25rem",
+								position: "absolute",
+								bottom: 0,
+								right: 0,
+								transform: "translate(25%, 25%)",
+							}}
+						>
+							{cartQuantity}
+						</div>
+					</Button>
+				)}
 			</Container>
 		</NavbarBs>
 	);
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utils/formatCurrency";
 
 type StoreItemProps = {
@@ -10,8 +11,17 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+	const {
+		getItemQuantity,
+		increaseItemQuantity,
+		decreaseItemQuantity,
+		removeItemFromCart,
+	} = useShoppingCart();
+
+	const quantity = getItemQuantity(id);
+
 	return (
-		<Card>
+		<Card className="h-100">
 			<Card.Img
 				style={{ objectFit: "cover" }}
 				variant="top"
@@ -23,6 +33,36 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
 					<span className="fs-2">{name}</span>
 					<span className="ms-2 text-muted">{formatCurrency(price)}</span>
 				</Card.Title>
+				<div className="mt-auto">
+					{quantity === 0 ? (
+						<Button onClick={() => increaseItemQuantity(id)} className="w-100">
+							Add to Cart
+						</Button>
+					) : (
+						<div
+							className="d-flex align-items-center flex-column"
+							style={{ gap: "0.5rem" }}
+						>
+							<div
+								className="d-flex align-items-center justify-content-center"
+								style={{ gap: "0.5rem" }}
+							>
+								<Button onClick={() => decreaseItemQuantity(id)}>-</Button>
+								<div>
+									<span className="fs-3">{quantity} </span>in cart
+								</div>
+								<Button onClick={() => increaseItemQuantity(id)}>+</Button>
+							</div>
+							<Button
+								onClick={() => removeItemFromCart(id)}
+								variant="danger"
+								size="sm"
+							>
+								Remove
+							</Button>
+						</div>
+					)}
+				</div>
 			</Card.Body>
 		</Card>
 	);
